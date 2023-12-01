ffe1920581e758405ff8dc3e635f19cb0a9d99a2
ext-iortgar <ivan.ortega.searchitmail@gmail.com>
2023-12-01 10:28:59 +0100

    Fin de sección 4

---
+++ src/app/heroes/components/hero/hero.component.ts
@@ -0,0 +1,37 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-heroes-hero',
+  templateUrl: './hero.component.html',
+  styleUrls: ['./hero.component.css']
+})
+export class HeroComponent {
+
+  public name: string = 'ironMan';
+  public age: number = 45;
+
+  get capitalizedName():string{
+    return this.name.toUpperCase();
+  }
+
+  getHeroDescription():string{
+    return `${this.name } - ${this.age}`;
+  }
+
+  changeName():void{
+    if(this.name.toUpperCase() == 'IRONMAN')
+      this.name = 'Spiderman';
+    else
+      this.name = "ironMan"
+  }
+
+  changeAge():void{
+    this.age = 25;
+  }
+
+  resetForm():void{
+    this.name = "ironMan";
+    this.age = 45;
+  }
+
+}
